934735875e44c12a4b0f4604a26cb463430ce0bf
jquerycods <cizgifilmhikayeleri@gmail.com>
2016-08-12 20:57:31 +0300

    Add files via upload

---
+++ popout.js
@@ -0,0 +1,18 @@
+﻿jQuery(document).ready(function(){
+jQuery("#fb_right").hover(function(){
+jQuery(this).stop(true,false).animate({right:  0}, 500);
+ },function(){ jQuery("#fb_right").stop(true,false).animate({right: -250}, 500);
+ });
+   jQuery("#tweet_right").hover(function(){
+ jQuery(this).stop(true,false).animate({right:  0}, 500);
+ },function(){ jQuery("#tweet_right").stop(true,false).animate({right: -250}, 500); });
+jQuery("#plus_google_right").hover(function(){
+ jQuery(this).stop(true,false).animate({right:  0}, 500);
+ },function(){ jQuery("#plus_google_right").stop(true,false).animate({right: -80}, 500); });
+    jQuery("#abone_right").hover(function(){
+ jQuery(this).stop(true,false).animate({right:  0}, 500);
+ },function(){ jQuery("#abone_right").stop(true,false).animate({right: -303}, 500); });
+    });
+// Blogger Eklentileri URL=(http://bloggereklentileri.blogspot.com.tr/)
+// Çizgi Film Hikayeleri URL=(http://cizgifilmhikayeleri.blogspot.com.tr/)
+// Blogger Grupları URL=(https://plus.google.com/u/0/+BloggerGruplar%C4%B1)
